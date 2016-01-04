83a0916584a8e975827cb13cd4cbccd8f31c5f7c
Yoshio Nose <ynose.249@gmail.com>
2016-01-04 19:08:25 +0900

    開始日を曜日によって色を変える

--- app/src/main/java/com/ynoseapps/eventmap/EventAdapter.java
+++ app/src/main/java/com/ynoseapps/eventmap/EventAdapter.java
@@ -1,6 +1,7 @@
 package com.ynoseapps.eventmap;
 
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,8 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Created by yoshio on 15/12/25.
@@ -16,7 +19,7 @@ public class EventAdapter extends BaseAdapter {
 
     Context context;
     LayoutInflater layoutInflater = null;
-    ArrayList<Event> eventList;
+    ArrayList<Event> events;
 
     public EventAdapter (Context context) {
         this.context = context;
@@ -24,30 +27,60 @@ public class EventAdapter extends BaseAdapter {
     }
 
     public void setEventList(ArrayList<Event> eventList) {
-        this.eventList = eventList;
+        this.events = eventList;
     }
 
     @Override
     public int getCount() {
-        return eventList.size();
+        return events.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return eventList.get(position);
+        return events.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return eventList.get(position).getId();
+        return events.get(position).getId();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         convertView = layoutInflater.inflate(R.layout.event_row, parent, false);
 
-        ((TextView)convertView.findViewById(R.id.title)).setText(eventList.get(position).title);
-        ((TextView)convertView.findViewById(R.id.startAt)).setText(eventList.get(position).getStartAtFormatedString());
+        Event event = events.get(position);
+
+        // イベントタイトル
+        ((TextView)convertView.findViewById(R.id.title)).setText(event.title);
+
+
+        // 開始日時
+        TextView startAt = ((TextView) convertView.findViewById(R.id.startAt));
+        startAt.setText(event.getStartAtFormatedString());
+
+        // 曜日によって色を変える
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(event.startAt);
+        GregorianCalendar calendarStartAt = new GregorianCalendar(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        int startAtColor;
+        switch (calendarStartAt.get(Calendar.DAY_OF_WEEK)) {
+            case Calendar.SATURDAY:
+                startAtColor = R.color.colorAtartAtSaturday;
+                break;
+            case Calendar.SUNDAY:
+                startAtColor = R.color.colorAtartAtSunday;
+                break;
+            default:
+                startAtColor = R.color.colorAtartAt;
+                break;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            startAt.setTextColor(context.getResources().getColor(startAtColor, null));
+        } else {
+            startAt.setTextColor(context.getResources().getColor(startAtColor));
+        }
 
         return convertView;
     }
